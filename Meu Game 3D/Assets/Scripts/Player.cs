37dbd3d1e66fff52fe3bbacbca7ec2cf90b591ee
Theprefeito <soares.saymon@escolar.ifrn.edu.br>
2024-08-19 16:08:50 -0300

    added player movement

---
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int velocidade = 10;
+    private Rigidbody rb; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("START");
+        TryGetComponent(out rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("UPDATE");
+        float h = Input.GetAxis("Horizontal"); // -1 esquerda, 0 nada, 1 direita
+        float v = Input.GetAxis("Vertical"); // -1 pra trás, 0 nada, 1 pra frente
+
+        Vector3 direcao = new Vector3(h, 0, v );
+        rb.AddForce(direcao * velocidade * Time.deltaTime);
+    }
+}
