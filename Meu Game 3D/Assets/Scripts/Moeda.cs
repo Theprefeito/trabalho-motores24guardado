071aef25f91bf6d23556bb3eb73db45d3f30c860
Theprefeito <soares.saymon@escolar.ifrn.edu.br>
2024-08-25 22:58:24 -0300

    GameAlmostFinished
    Added pretty much all the necessary features for the game to work properly. Work on the level design to go.

---
+++ Meu Game 3D/Assets/Scripts/Moeda.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Moeda : MonoBehaviour
+{
+
+    public int velocidadeGiro = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().SubtrairMoedas(valor:1);
+            Destroy(gameObject);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(UnityEngine.Vector3.up * velocidadeGiro * Time.deltaTime, relativeTo:Space.World);
+
+    }
+}
--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,25 +6,66 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    public int velocidade = 10;
+    public int velocidade = 20;
+    public int forcaPulo = 7;
+    public bool onFloor;
+    
+    
+    
     private Rigidbody rb; 
+    private AudioSource source;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("START");
         TryGetComponent(out rb);
+        TryGetComponent(out source);
     }
+
+
+void OnCollisionEnter(Collision collision)
+{
+    if(!onFloor && collision.gameObject.tag == "Floor")
+    {
+        onFloor = true;
+    }
+}
+
+
+
 
     // Update is called once per frame
     void Update()
     {
+        //movimentação do jogador
         Debug.Log("UPDATE");
         float h = Input.GetAxis("Horizontal"); // -1 esquerda, 0 nada, 1 direita
         float v = Input.GetAxis("Vertical"); // -1 pra trás, 0 nada, 1 pra frente
 
         Vector3 direcao = new Vector3(h, 0, v );
         rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
+        
+        
+        //pulo
+        
+        Vector3 jumpForce = new Vector3(0, 1, 0);
+        if(Input.GetKeyDown(KeyCode.Space) && onFloor)
+        {
+           source.Play();
+           
+           rb.AddForce(jumpForce * forcaPulo, ForceMode.Impulse);
+            onFloor = false;
+        }
 
+
+
+
+
+
+        //resetar por circunstâmcia
         if(transform.position.y <= - 15)
         {
             //jogador caiu
